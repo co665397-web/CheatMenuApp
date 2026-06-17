41324de7d10815ff6b605d14391ba8212eb97890
co665397-web <co665397@gmail.com>
2026-06-17 20:18:08 +0700

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+    }
+}
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
